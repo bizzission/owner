63711300d4004fe47d71f5524b88fcd61427b513
railken <railken@protonmail.ch>
2019-03-19 17:15:18 +0100

    update relations

--- src/Models/Ownable.php
+++ src/Models/Ownable.php
@@ -3,6 +3,8 @@
 namespace Railken\Amethyst\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Railken\Amethyst\Common\ConfigurableModel;
 use Railken\Lem\Contracts\EntityContract;
@@ -25,7 +27,7 @@ class Ownable extends Model implements EntityContract
     /**
      * @return \Illuminate\Database\Eloquent\Relations\MorphTo
      */
-    public function owner()
+    public function owner(): MorphTo
     {
         return $this->morphTo();
     }
@@ -33,7 +35,7 @@ class Ownable extends Model implements EntityContract
     /**
      * @return \Illuminate\Database\Eloquent\Relations\MorphTo
      */
-    public function ownable()
+    public function ownable(): MorphTo
     {
         return $this->morphTo();
     }
