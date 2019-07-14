728a3144a7fc90bc12d66badf38fef00cf5645ec
railken <railken@protonmail.ch>
2019-07-14 13:10:38 +0200

    fix style

--- src/Managers/OwnableManager.php
+++ src/Managers/OwnableManager.php
@@ -6,12 +6,12 @@ use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
 /**
- * @method \Amethyst\Models\Ownable newEntity()
- * @method \Amethyst\Schemas\OwnableSchema getSchema()
+ * @method \Amethyst\Models\Ownable                 newEntity()
+ * @method \Amethyst\Schemas\OwnableSchema          getSchema()
  * @method \Amethyst\Repositories\OwnableRepository getRepository()
- * @method \Amethyst\Serializers\OwnableSerializer getSerializer()
- * @method \Amethyst\Validators\OwnableValidator getValidator()
- * @method \Amethyst\Authorizers\OwnableAuthorizer getAuthorizer()
+ * @method \Amethyst\Serializers\OwnableSerializer  getSerializer()
+ * @method \Amethyst\Validators\OwnableValidator    getValidator()
+ * @method \Amethyst\Authorizers\OwnableAuthorizer  getAuthorizer()
  */
 class OwnableManager extends Manager
 {
