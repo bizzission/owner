d8818fabd99ab5a4544d285442fd0ec190ae0b2e
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/OwnableManager.php
+++ src/Managers/OwnableManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Ownable newEntity()
+ * @method \Amethyst\Schemas\OwnableSchema getSchema()
+ * @method \Amethyst\Repositories\OwnableRepository getRepository()
+ * @method \Amethyst\Serializers\OwnableSerializer getSerializer()
+ * @method \Amethyst\Validators\OwnableValidator getValidator()
+ * @method \Amethyst\Authorizers\OwnableAuthorizer getAuthorizer()
+ */
 class OwnableManager extends Manager
 {
     use ConfigurableManager;
