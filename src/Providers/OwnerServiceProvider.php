85b44748de47fce260904f92f2f3c5c4257eae3b
railken <railken@protonmail.ch>
2019-08-22 14:58:52 +0200

    improve

--- src/Providers/OwnerServiceProvider.php
+++ src/Providers/OwnerServiceProvider.php
@@ -25,34 +25,39 @@ class OwnerServiceProvider extends CommonServiceProvider
         }
 
         if (Schema::hasTable(Config::get('amethyst.owner.data.ownable.table'))) {
-            Event::listen(['eloquent.created: *'], function ($event_name, $events) {
-                $class = explode(': ', $event_name)[1];
+            Event::listen(['eloquent.created: *', 'eloquent.deleted: *'], function ($event_name, $events) {
+                $model = $events[0];
+                $class = get_class($model);
 
                 if ($class === Ownable::class) {
                     return;
                 }
 
-                $model = $events[0];
-
                 $owner = null;
 
                 try {
                     $manager = app('amethyst')->newManagerByModel($class);
-                    $owner = $manager->getHistory($model->id);
+
+                    $owner = $manager->getHistory();
                 } catch (\Exception $e) {
                 }
 
-                if (!$owner) {
+                if (!$owner || $owner instanceof \Railken\Lem\Agents\SystemAgent) {
                     return;
                 }
 
-                Ownable::create([
+                [$eventType, $class] = explode(": ", $event_name);
+
+
+                $parameters = [
                     'owner_type'   => app('amethyst')->tableize($owner),
                     'owner_id'     => $owner->id,
                     'relation'     => 'author',
                     'ownable_type' => app('amethyst')->tableize($model),
                     'ownable_id'   => $model->id,
-                ]);
+                ];
+
+                $eventType === 'eloquent.created' ? Ownable::create($parameters) : Ownable::where($parameters)->delete();
             });
         }
     }
